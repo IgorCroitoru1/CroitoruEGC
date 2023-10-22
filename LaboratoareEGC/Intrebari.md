70d53b94b1c96a628225873550bef94eea070d50
Igor Croitoru <croitoru-2013@mail.ru>
2023-10-22 13:32:48 +0300

    Add project files.

---
+++ LaboratoareEGC/Intrebari.md
@@ -0,0 +1,26 @@
+﻿1. Ce este un viewport?
+Un viewport se referă la o regiune rectangulară a suprafeței de afișare (cum ar fi ecranul unui calculator sau o fereastră de vizualizare) în care
+sunt desenate obiectele grafice.
+
+2. Ce reprezintă conceptul de frames per seconds din punctul de vedere al bibliotecii OpenGL?
+FPS (Frames Per Second) în OpenGL reprezintă numărul de cadre pe secundă desenate și afișate pe ecran. 
+Cu cât acest număr este mai mare, cu atât animațiile par mai fluide.
+
+3. Ce este modul imediat de randare?
+Modul imediat de randare (Immediate Mode Rendering) este o abordare în OpenGL în care desenarea se face printr-un set de apeluri directe la funcții OpenGL pentru fiecare vertex și poligon.
+Este considerat învechit și ineficient și nu este recomandat în versiunile mai noi de OpenGL.
+
+4. Care este ultima versiune de OpenGL care acceptă modul imediat?
+Ultima versiune de OpenGL care acceptă modul imediat este OpenGL 3.0.
+
+5. Când este rulată metoda OnRenderFrame()?
+Metoda OnRenderFrame() este rulată în cadrul buclei principale de randare și este utilizată pentru desenarea graficii pe ecran.
+Aici, se efectuează toate operațiunile de desenare a obiectelor.
+
+6. De ce este nevoie ca metoda OnResize() să fie executată cel puțin o dată?
+Metoda OnResize() trebuie să fie executată cel puțin o dată pentru a inițializa viewport-ul și pentru a se asigura că obiectele desenate vor avea coordonatele corecte în contextul noii dimensiuni a ferestrei.
+
+7. Ce reprezintă parametrii metodei CreatePerspectiveFieldOfView() și care este domeniul de valori pentru aceștia?
+Metoda CreatePerspectiveFieldOfView() este folosită pentru a crea o matrice de proiecție perspectivă în OpenGL. 
+Parametrii acestei metode reprezintă unghiul de câmp de vedere vertical (fovy), raportul de aspect (aspect ratio), distanța la planul de apropiere (zNear), și distanța la planul de depărtare (zFar).
+Valorile pentru acești parametri variază în funcție de cerințele aplicației, dar în general, fovy este un unghi în radiani, aspect este raportul dintre lățimea și înălțimea ferestrei, iar zNear și zFar reprezintă distanța la planurile de apropiere și depărtare.
---
+++ LaboratoareEGC/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace Lab2
+{
+    class SimpleWindow : GameWindow
+    {
+        private Vector2 squarePosition = new Vector2(0.0f, 0.0f); // Poziția pătratului
+        private float rotationAngle = 0.0f; // Unghiul de rotație al pătratului
+        private float rotationSpeed = 2.0f; // Viteza de rotație
+
+        public SimpleWindow() : base(800, 600, new GraphicsMode(32, 24, 0, 8))
+        {
+            KeyDown += Keyboard_KeyDown; // Ascultă evenimentele tastelor apăsate
+            MouseMove += Mouse_Move; // Ascultă evenimentele de mișcare a mouse-ului
+        }
+
+        void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Exit(); // Ieșirea din aplicație la apăsarea tastei Esc
+
+            // Mută pătratul cu tastele săgeată stânga/dreapta
+            if (e.Key == Key.Left)
+                squarePosition.X -= 0.05f;
+            if (e.Key == Key.Right)
+                squarePosition.X += 0.05f;
+        }
+
+        void Mouse_Move(object sender, MouseMoveEventArgs e)
+        {
+            // Roțește pătratul în funcție de mișcarea mouse-ului
+            rotationAngle += e.XDelta * rotationSpeed;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0.350f, 0.245f, 0.245f, 0); // Setarea culorii de fundal a ferestrei
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height); // Setarea zonei de afișare OpenGL
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0); // Definirea sistemului de coordonate
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            // Logica de actualizare (dacă este necesară)
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit); // Șterge buffer-ul de culoare
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            // Aplică transformările asupra pătratului
+            GL.Translate(squarePosition.X, squarePosition.Y, 0.0);
+            GL.Rotate(rotationAngle, 0.0, 0.0, 1.0);
+
+            // Desenarea pătratului
+            GL.Begin(PrimitiveType.Quads);
+            GL.Color3(0.622f, 0.840f, 0.822f); // Setarea culorii pătratului
+            GL.Vertex2(-0.1f, -0.1f); // Definirea vârfurilor pătratului
+            GL.Vertex2(0.1f, -0.1f);
+            GL.Vertex2(0.1f, 0.1f);
+            GL.Vertex2(-0.1f, 0.1f);
+            GL.End();
+
+            this.SwapBuffers(); // Efectuarea schimbului de buffer pentru afișare
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (SimpleWindow example = new SimpleWindow())
+            {
+                example.Run(30.0, 0.0); // Rularea aplicației cu un anumit framerate
+            }
+        }
+    }
+}
